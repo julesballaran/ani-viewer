5e1b4bbafdcc0f36163f7645a01d09359dbad40c
Jules Ballaran <jules.ballaran@boom.camp>
2022-01-24 15:53:16 +0800

    update proxy server

--- utils/constants.js
+++ utils/constants.js
@@ -1,5 +1,5 @@
 export const CORS = "https://cors-link.herokuapp.com"
 export const BASEURL = `${CORS}/https://www4.gogoanime.cm`
-export const PROXY = `${CORS}/https://cors-link.herokuapp.com/proxy`
+export const PROXY = "https://ani-proxy.herokuapp.com"
 export const USERAGENT =
   "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
